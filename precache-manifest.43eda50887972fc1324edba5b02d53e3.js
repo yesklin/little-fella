72665029e73709353cb3409ec370b3e500782a14
yesklin <cd.santos.360@gmail.com>
2020-04-16 15:42:59 -0300

    Updates

---
+++ precache-manifest.43eda50887972fc1324edba5b02d53e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "731d5d3161189ebd890fad7517f83812",
+    "url": "/little-fella/index.html"
+  },
+  {
+    "revision": "961f7cc17dcd304c61b0",
+    "url": "/little-fella/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f5f7fbf2a502f4e31599",
+    "url": "/little-fella/static/js/2.88bb4ca4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/little-fella/static/js/2.88bb4ca4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "961f7cc17dcd304c61b0",
+    "url": "/little-fella/static/js/main.2b73be28.chunk.js"
+  },
+  {
+    "revision": "2ad627e9da3543dba089",
+    "url": "/little-fella/static/js/runtime-main.a4812606.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/little-fella/static/media/logo.5d5d9eef.svg"
+  }
+]);
