872f11b13532aefb1bc0e063d6611109f783c940
yesklin <cd.santos.360@gmail.com>
2020-04-16 15:48:35 -0300

    testing

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello, from React
         </a>
       </header>
     </div>
