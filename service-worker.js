f4e6635ba02176f20f94dbf705383ad28d502603
yesklin <cd.santos.360@gmail.com>
2020-04-16 22:41:05 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/little-fella/precache-manifest.37ec4a92d115571d574f6b19425aba12.js"
+  "/little-fella/precache-manifest.7b8e522aaf00b51e116a3a3928c4b5dc.js"
 );
 
 self.addEventListener('message', (event) => {
